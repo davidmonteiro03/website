ad743eb987da262f8b81d570b7a40e99ee19e42f
Your Name <you@example.com>
2024-06-03 07:11:11 +0100

    git updates

--- docker/srcs/requirements/website/tools/website/auth/models.py
+++ docker/srcs/requirements/website/tools/website/auth/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+	id = models.AutoField(primary_key=True)
+	fname = models.CharField(max_length=255)
+	lname = models.CharField(max_length=255)
+	username = models.CharField(max_length=255)
+	email = models.EmailField(max_length=255)
+	password = models.CharField(max_length=255)
