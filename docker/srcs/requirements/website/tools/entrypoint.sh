b90283ec9d089ccd2c5216419a7523779f272a7c
David Monteiro <damonteiro12446@gmail.com>
2024-06-07 12:56:39 +0100

    some fixes

--- docker/srcs/requirements/website/tools/entrypoint.sh
+++ docker/srcs/requirements/website/tools/entrypoint.sh
@@ -6,14 +6,14 @@
 #    By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/06/02 08:42:25 by dcaetano          #+#    #+#              #
-#    Updated: 2024/06/05 15:18:41 by dcaetano         ###   ########.fr        #
+#    Updated: 2024/06/07 12:56:00 by dcaetano         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/sh
 
 cd /website
-python3 manage.py makemigrations authentication
+python3 manage.py makemigrations backend
 python3 manage.py migrate
 python3 manage.py collectstatic --noinput
 python3 manage.py runserver 0.0.0.0:5000
