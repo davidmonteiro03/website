3f5b11d0e0200c86c3d00422d338c0108332dc94
David Monteiro <damonteiro12446@gmail.com>
2024-06-18 15:07:56 +0100

    working on helpful instructions

--- setup.sh
+++ setup.sh
@@ -6,13 +6,13 @@
 #    By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/06/05 13:38:01 by dcaetano          #+#    #+#              #
-#    Updated: 2024/06/18 15:07:00 by dcaetano         ###   ########.fr        #
+#    Updated: 2024/06/18 15:07:46 by dcaetano         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/sh
 
-< $PWD/docker/srcs/.env
+< ./docker/srcs/.env
 if [ $? -ne 0 ]; then
 	echo "You need to create a .env file in the ./docker/srcs folder"
 	exit 1
