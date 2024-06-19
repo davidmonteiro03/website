9a41bb0edbff3ff7557265ab0996a4f381251e72
David Monteiro <damonteiro12446@gmail.com>
2024-06-19 12:23:31 +0100

    git updates

--- docker/srcs/requirements/website/tools/entrypoint.sh
+++ docker/srcs/requirements/website/tools/entrypoint.sh
@@ -6,7 +6,7 @@
 #    By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/06/02 08:42:25 by dcaetano          #+#    #+#              #
-#    Updated: 2024/06/19 10:25:02 by dcaetano         ###   ########.fr        #
+#    Updated: 2024/06/19 12:14:15 by dcaetano         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,7 +14,7 @@
 output="from django.contrib.auth.models import User\n\
 User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME','$DJANGO_SUPERUSER_EMAIL','$DJANGO_SUPERUSER_PASSWORD')"
 cd /website
-python manage.py makemigrations api auth
+python manage.py makemigrations api authentication
 python manage.py migrate
 echo $output | python3 manage.py shell
 python manage.py collectstatic --noinput
