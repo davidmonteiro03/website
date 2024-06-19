e9115c8c50a7e6af0bc4709ce547807787b18efc
David Monteiro <damonteiro12446@gmail.com>
2024-06-19 10:19:52 +0100

    api.js not necessary, it only requires some data in POST method

--- docker/srcs/requirements/website/tools/website/static/js/api.js
+++
@@ -1,8 +0,0 @@
-async function loadApiData(event, page) {
-	event.preventDefault();
-	const response = await fetch(`/api/${page}/`);
-	if (!response.ok) return;
-	const content = await response.json();
-	apiData = content[page];
-	changePage(event, page);
-}
