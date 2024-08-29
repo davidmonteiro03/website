cb1d2d98db3c57e7ee3a68b41e1ebcb50e2a3bc9
davidmonteiro03 <damonteiro12446@gmail.com>
2024-08-29 15:45:55 +0100

    git updates

--- docker/srcs/requirements/website/tools/entrypoint.sh
+++ docker/srcs/requirements/website/tools/entrypoint.sh
@@ -6,7 +6,7 @@
 #    By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/06/02 08:42:25 by dcaetano          #+#    #+#              #
-#    Updated: 2024/08/29 15:38:20 by dcaetano         ###   ########.fr        #
+#    Updated: 2024/08/29 15:43:51 by dcaetano         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,6 +15,6 @@
 cd /website
 python manage.py makemigrations api user
 python manage.py migrate
-python manage.py collectstatic --noinput
 python manage.py createsuperuser --noinput
+python manage.py collectstatic --noinput
 python manage.py runserver 0.0.0.0:$PORT
